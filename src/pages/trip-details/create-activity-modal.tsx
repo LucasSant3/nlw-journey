ae3e714209c9a3e997135332abb036427da6a599
Lucas <lucassantoslac3@gmail.com>
2024-07-13 21:57:36 -0300

    segundo commit, aula-02

---
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -0,0 +1,51 @@
+import { Calendar, Tag, X } from "lucide-react";
+import { Button } from "../../components/button";
+
+interface CreateActivityModalProps{
+  closeCreateActivityModal: () => void
+}
+
+export function CreateActivityModal({ closeCreateActivityModal }: CreateActivityModalProps) {
+  return(
+    <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='flex items-center justify-between'>
+              <h2 className='text-lg font-semibold'>Cadastrar Atividade</h2>
+              <button type='button' onClick={closeCreateActivityModal}>
+                <X className='size-5 text-zinc-400' />
+              </button>
+            </div>
+            <p className='text-zinc-400 text-sm mt-2'>
+              Todos os convidados podem visualizar as atividades.
+            </p>
+
+            <form className='space-y-3'>
+              <div className='h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center'>
+                <Tag className='text-zinc-400 mr-1' />
+                <input
+                  type="title"
+                  name='name'
+                  placeholder="Qual a atividade"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+              </div>
+
+              <div className="flex items-center gap-2">
+                <div className='h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center'>
+                  <Calendar className='text-zinc-400 mr-1' />
+                  <input
+                    type="datetime-local"
+                    name='occurs_at'
+                    placeholder="Data e Horário da Atividade"
+                    className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+                </div>
+              </div>
+
+              <Button size="full" variant="primary">
+                Salvar Atividade
+              </Button>
+
+            </form>
+          </div>
+        </div>
+  )
+}
